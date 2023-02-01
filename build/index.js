e47aee84c9d39b0c6b67082d0b598c3d191cba1e
arquimedesGa <gabox2050@gmail.com>
2023-02-01 14:44:45 -0300

    testing TS

--- build/index.js
+++ build/index.js
@@ -1,2 +1,16 @@
 "use strict";
-console.log("hola mun");
+Object.defineProperty(exports, "__esModule", { value: true });
+let name = 'Arquimedes';
+let age = 32;
+let working = false;
+let skill = ['proactive', 'dadicate', 'apassionate'];
+const variableArrayType = ['Arquimedes', 32, false];
+var timeWithoutJob;
+(function (timeWithoutJob) {
+    timeWithoutJob["years"] = "a";
+    timeWithoutJob["decade"] = "e";
+    timeWithoutJob["century"] = "i";
+})(timeWithoutJob || (timeWithoutJob = {}));
+;
+let timeAge = timeWithoutJob.century;
+console.log(timeAge);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,41 @@
-console.log("hola mun")
+let name: string = 'Arquimedes';
+
+let age: number = 32;
+
+let working: boolean = false;
+
+let skill: string[] = ['proactive', 'dadicate', 'apassionate'];
+
+let listOFnummber: number[] = [1, 2, 3, 4, 5, 6, 7];
+
+const variableArrayType:(string | number | boolean)[] = ['Arquimedes', 32, false];
+
+//type of enum
+
+enum monthOfYear  {
+    january = 1,
+    february,
+    march,
+    april,
+    may,
+    june,
+    july,
+    august,
+    september,
+    october,
+    novemeber,
+    december
+};
+
+enum daysOfWeek {
+    monday = 1,
+    tuesday,
+    wendnesday,
+    thursday,
+    friday, 
+    saturday,
+    sunday
+};
+
+
+console.log(`${name} have a interview at monday ${daysOfWeek.monday} with a big company`);
